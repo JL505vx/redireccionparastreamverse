7df31a13671295333349d6a648f9a65a9f11fabd
JL505vx <yisuselboss13@gmail.com>
2026-05-11 23:53:18 -0600

    Configurar hosts permitidos

--- redirector/settings.py
+++ redirector/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%h)++nyk7=1va)y&u1eu+n#w7tg$psed*wzga!pb(qv!@c)l+f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
